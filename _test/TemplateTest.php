7cad8c88af426b82be77e834b025f42b35ef368e
Andreas Gohr <andi@splitbrain.org>
2026-08-07 10:05:47 +0200

    remove test fixture from the shipped default template
    The placeholder test read tpl/default/unittest.html, which put test data
    inside the official template directory. Placeholder replacement is now
    tested directly via callInaccessibleMethod(), while file lookup and the
    fallback to the generic variant are covered against the real template
    files. The fixture was added in 9220075.

--- _test/TemplateTest.php
+++ _test/TemplateTest.php
@@ -2,7 +2,6 @@
 
 namespace dokuwiki\plugin\dw2pdf\test;
 
-use dokuwiki\plugin\dw2pdf\src\AbstractCollector;
 use dokuwiki\plugin\dw2pdf\src\Config;
 use dokuwiki\plugin\dw2pdf\src\PageCollector;
 use dokuwiki\plugin\dw2pdf\src\Template;
@@ -14,6 +13,9 @@ use DokuWikiTest;
  */
 class TemplateTest extends DokuWikiTest
 {
+    /** @var string The page ID used as context in all tests */
+    protected string $pageid = 'playground:templatepage';
+
     public function setUp(): void
     {
         parent::setUp();
@@ -21,13 +23,15 @@ class TemplateTest extends DokuWikiTest
     }
 
     /**
-     * Template placeholders (title, username, QR codes, links) should be expanded from context data.
+     * Create a template with a page context set up
+     *
+     * @return Template
      */
-    public function testPlaceholderReplacement(): void
+    protected function getTemplate(): Template
     {
-        global $ID, $INPUT, $conf;
+        global $ID, $INPUT;
         $INPUT->set('book_title', 'Export Title');
-        $ID = 'playground:templatepage';
+        $ID = $this->pageid;
         saveWikiText($ID, 'template test', 'create');
 
         $config = new Config([
@@ -38,7 +42,34 @@ class TemplateTest extends DokuWikiTest
         $template = new Template($config);
         $template->setContext($collector, $ID, 'username');
 
-        $html = $template->getHTML('unittest');
+        return $template;
+    }
+
+    /**
+     * Template placeholders (title, username, QR codes, links) should be expanded from context data.
+     */
+    public function testPlaceholderReplacement(): void
+    {
+        global $conf;
+
+        $template = $this->getTemplate();
+        $html = self::callInaccessibleMethod($template, 'replacePlaceholders', [
+            "Page Number: @PAGE@\n" .
+            "Total Pages: @PAGES@\n" .
+            "Document Title: @TITLE@\n" .
+            "Wiki Title: @WIKI@\n" .
+            "Wiki URL: @WIKIURL@\n" .
+            "Date: @DATE@\n" .
+            "User: @USERNAME@\n" .
+            "Base Path: @BASE@\n" .
+            "Include Dir: @INC@\n" .
+            "Template Base Path: @TPLBASE@\n" .
+            "Template Include Dir: @TPLINC@\n" .
+            "Page ID: @ID@\n" .
+            "Revision: @UPDATE@\n" .
+            "Page URL: @PAGEURL@\n" .
+            "QR Code: @QRCODE@\n"
+        ]);
 
         $this->assertStringContainsString('Page Number: {PAGENO}', $html);
         $this->assertStringContainsString('Total Pages: {nbpg}', $html);
@@ -51,15 +82,49 @@ class TemplateTest extends DokuWikiTest
         $this->assertStringContainsString('Include Dir: ' . DOKU_INC, $html);
         $this->assertStringContainsString('Template Base Path: ' . DOKU_BASE . 'lib/plugins/dw2pdf/tpl/default/', $html);
         $this->assertStringContainsString('Template Include Dir: ' . DOKU_INC . 'lib/plugins/dw2pdf/tpl/default/', $html);
-        $this->assertStringContainsString('Page ID: ' . $ID, $html);
+        $this->assertStringContainsString('Page ID: ' . $this->pageid, $html);
 
-        $revisionDate = dformat(filemtime(wikiFN($ID)));
+        $revisionDate = dformat(filemtime(wikiFN($this->pageid)));
         $this->assertStringContainsString('Revision: ' . $revisionDate, $html);
 
-        $pageUrl = wl($ID, [], true, '&');
+        $pageUrl = wl($this->pageid, [], true, '&');
         $this->assertStringContainsString('Page URL: ' . $pageUrl, $html);
         $this->assertStringContainsString('<barcode', $html);
         $this->assertStringContainsString('size="1.5"', $html);
         $this->assertStringNotContainsString('@QRCODE@', $html);
+    }
+
+    /**
+     * The ordered variant of a template file should be preferred and have its placeholders replaced.
+     */
+    public function testOrderedFile(): void
+    {
+        $template = $this->getTemplate();
+
+        $html = $template->getHTML('header', 'odd');
+
+        $this->assertStringContainsString('class="pdfheader"', $html);
+        $this->assertStringContainsString('{PAGENO}/{nbpg}', $html);
+        $this->assertStringContainsString('Export Title', $html);
+    }
+
+    /**
+     * A missing ordered variant should fall back to the generic template file.
+     */
+    public function testFallbackToGenericFile(): void
+    {
+        $template = $this->getTemplate();
+
+        $this->assertSame($template->getHTML('citation'), $template->getHTML('citation', 'first'));
+    }
+
+    /**
+     * A template file that does not exist should result in empty output.
+     */
+    public function testMissingFile(): void
+    {
+        $template = $this->getTemplate();
+
+        $this->assertSame('', $template->getHTML('cover'));
     }
 }
